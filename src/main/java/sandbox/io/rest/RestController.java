b754d531d06514f159e8af6eec88d4fcf03389f6
r-uu <roger.wegner@web.de>
2022-04-19 17:55:53 +0200

    initial

---
+++ src/main/java/sandbox/io/rest/RestController.java
@@ -0,0 +1,28 @@
+package sandbox.io.rest;
+
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
+
+import org.eclipse.microprofile.openapi.annotations.Operation;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Response;
+
+@ApplicationScoped
+public class RestController
+{
+	@GET
+	@Path("/system/properties")
+	@Produces(APPLICATION_JSON)
+	@Operation
+	(
+			summary = "Get JVM system properties for particular host",
+			description = "Retrieves and returns the JVM system properties from the system service running on the particular host."
+	)
+	public Response getSystemProperties()
+	{
+		return Response.ok(System.getProperties()).build();
+	}
+}
